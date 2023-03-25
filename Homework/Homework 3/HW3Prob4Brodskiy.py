6bcc992031dc61ccb7d9a84ccb4f95ac0bb7c533
Michael Brodskiy <Michael@Brodskiy.com>
2023-03-25 14:31:54 -0400

    finished hw 3

--- Homework/Homework 3/HW3Prob4Brodskiy.py
+++ Homework/Homework 3/HW3Prob4Brodskiy.py
@@ -20,19 +20,41 @@
 
 def stringCalc(phrase):
 
-    units = [["meter", "gram", "liter", "foot", "pounds", "gallon"], [3.28, .0022, .22, .3048, 453.59, 4.546]]
-    tokens = phrase.lower().split()
-    (initialUnit, finalUnit) = ("", "")
+    modifiers = {"kilo":.001, "hecto":.01, "deka":.1, "":1, "deci":10, "centi":100, "milli":1000}
+    units = ["meter", "meters", "gram", "grams", "liter", "liters", "foot", "feet", "pound", "pounds", "gallon", "gallons"] 
+    factors = [.3048, .3048, 453.59, 453.59, 4.546, 4.546, 3.28, 3.28, .0022, .0022, .22, .22]
+    tokens = phrase.lower().replace("?","").split()
+    initialBaseUnit = ""
+    initialUnit = ""
     initialQuant = 0
+    initialModifier = ""
+    finalBaseUnit = ""
+    finalUnit = ""
+    finalModifier = ""
+    unitListLength = len(units)
 
     for i in tokens:
-        if i in units[0] and finalUnit == "":
-            finalUnit = i
-        elif i in units[0]:
-            initialUnit = i
+        for j in modifiers.keys():
+            for k in range(unitListLength):
+                if (i == (j + units[k]) and finalUnit == ""):
+                    finalBaseUnit = units[k]
+                    finalUnit = i
+                    finalModifier = j
+                elif (i == (j + units[k])):
+                    initialBaseUnit = units[k]
+                    initialUnit = i
+                    initialModifier = j
 
-    initialQuant = tokens[tokens.index(initialUnit) - 1]
+    if (abs(units.index(finalBaseUnit) - units.index(initialBaseUnit)) % 5 == 0):
+        initialQuant = 1.0
+    elif (abs(units.index(finalBaseUnit) - units.index(initialBaseUnit)) % 6 == 0): 
+        initialQuant = float(tokens[tokens.index(initialUnit) - 1])
+    else:
+        print("The conversion from", initialUnit, "to", finalUnit, "is invalid. Exiting...")
+        return "Error!"
 
-    return initialQuant * units[1][units[0].index(finalUnit)]
+    return f"There are {modifiers[finalModifier] * initialQuant * factors[units.index(finalBaseUnit)] / modifiers[initialModifier]:.4f} {finalUnit} in {initialQuant} {initialUnit}"
 
-print(stringCalc(input("Enter conversion phrase: ")))
+print("Enter a conversion phrase below.")
+print("Prefixes from milli- to kilo- are valid.")
+print(stringCalc(input("Available units include meters/feet, grams/pounds, and liters/gallons:  ")))
